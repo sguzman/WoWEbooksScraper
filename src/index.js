1aa9d391433818ebc453f61f55c5cdec18f3dc9a
Salvador Guzman <guzmansalv@gmail.com>
2018-03-16 12:52:22 -0700

    use rxjs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const rp = require('request-promise');
 const rxjs = require('rxjs');
+const args = require('minimist')(process.argv.slice(2));
 
 const page = require('./page');
 
-rp('http://www.wowebook.co/')
-.then(function(body) {
-  console.log(page(body));
-});
+rxjs.Observable.range(1, args.n)
+  .flatMap(t => rxjs.Observable.fromPromise(rp(`http://www.wowebook.co/page/${t}/`)))
+  .map(t => page(t))
+  .subscribe(t => console.log(t));
